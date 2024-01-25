f83a2813e1d8928000bfbacd2a1f9a645e16efd8
Oscar Olsen <user.name>
2024-01-24 23:36:45 -0600

    docs: removed content in todo.txt

--- src/shared/feature_get_path_pem_file/feature.py
+++ src/shared/feature_get_path_pem_file/feature.py
@@ -2,12 +2,12 @@
 Module resposibility:
 """
 
+# pylint:disable = C0116
+
 import os
 
 
 def get_path_pem_file(pem_file_name: str) -> str:
-    # pylint:disable = C0116
-
     directory_path = os.getenv("DIRECTORY_PATH_PEM_FILE")
 
     path = f"{directory_path}\\{os.getenv(pem_file_name)}"
